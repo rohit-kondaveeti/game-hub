f610863e0ba7ae394e83171d4ad07aaabd20f872
veerababu-ch <kondaveetirohit@gmail.com>
2023-08-01 08:14:55 +0530

    added search bar

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,9 @@
+import { Input } from "@chakra-ui/react";
+
+const SearchInput = () => {
+  return (
+    <Input borderRadius={20} placeholder="Search Games..." variant="filled" />
+  );
+};
+
+export default SearchInput;
